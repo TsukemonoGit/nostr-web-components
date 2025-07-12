886c2122f75cd600625f432f51198066d3aee0e5
TsukemonoGit <mine43try@outlook.com>
2025-07-12 15:16:11 +0900

    fi

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import { sveltekit } from '@sveltejs/kit/vite';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 export default defineConfig({
 	plugins: [
 		tailwindcss(),
@@ -18,18 +18,18 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: {
-			'nostr-web-components': path.resolve(__dirname, 'src/lib') // 実際のパスに合わせて調整
+			'nostr-web-components': path.resolve(__dirname, 'src/lib')
 		}
 	},
 	build: {
 		lib: {
-			entry: 'src/lib/index.ts', // ライブラリのエントリーポイント
-			name: 'NostrWebComponents', // グローバル変数名（UMD形式用）
+			entry: 'src/lib/index.ts',
+			name: 'NostrWebComponents',
 			fileName: 'nostr-web-components',
 			formats: ['es', 'umd']
 		},
 		rollupOptions: {
-			external: ['svelte'], // 必要に応じて外部化するライブラリ
+			external: ['svelte'],
 			output: {
 				globals: {
 					svelte: 'Svelte'
