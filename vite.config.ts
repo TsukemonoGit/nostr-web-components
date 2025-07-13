bb64b63d2c94675f8aa4674e9b4d08079412e305
TsukemonoGit <mine43try@outlook.com>
2025-07-13 10:43:18 +0900

    f

--- vite.config.ts
+++ vite.config.ts
@@ -7,35 +7,50 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-export default defineConfig({
-	base: '/nostr-web-components/',
-	plugins: [
-		tailwindcss(),
-		svelte({
-			compilerOptions: {
-				customElement: true
-			}
-		})
-	],
-	resolve: {
-		alias: {
-			'nostr-web-components': path.resolve(__dirname, 'src/lib')
-		}
-	},
-	build: {
-		lib: {
-			entry: 'src/lib/index.ts',
-			name: 'NostrWebComponents',
-			fileName: 'nostr-web-components',
-			formats: ['es', 'umd']
-		},
-		rollupOptions: {
-			external: ['svelte'],
-			output: {
-				globals: {
-					svelte: 'Svelte'
+export default defineConfig(({ mode }) => {
+	switch (mode) {
+		case 'pages': //github actions でpagesをデプロイするときのやつ
+			return {
+				base: '/nostr-share-component/',
+				plugins: [
+					svelte({
+						compilerOptions: {
+							customElement: true
+						}
+					})
+				]
+			};
+		default:
+			return {
+				plugins: [
+					tailwindcss(),
+					svelte({
+						compilerOptions: {
+							customElement: true
+						}
+					})
+				],
+				resolve: {
+					alias: {
+						'nostr-web-components': path.resolve(__dirname, 'src/lib')
+					}
+				},
+				build: {
+					lib: {
+						entry: 'src/lib/index.ts',
+						name: 'NostrWebComponents',
+						fileName: 'nostr-web-components',
+						formats: ['es', 'umd']
+					},
+					rollupOptions: {
+						external: ['svelte'],
+						output: {
+							globals: {
+								svelte: 'Svelte'
+							}
+						}
+					}
 				}
-			}
-		}
+			};
 	}
 });
