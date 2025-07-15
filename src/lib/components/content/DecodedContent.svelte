e40b94da50e67f01f9a43ef34a7a21518b6692b6
TsukemonoGit <mine43try@outlook.com>
2025-07-15 13:10:28 +0900

    fix inline

--- src/lib/components/content/DecodedContent.svelte
+++ src/lib/components/content/DecodedContent.svelte
@@ -15,14 +15,23 @@
 	let { decoded, themeClass, theme, part, display }: Props = $props();
 </script>
 
-{#if decoded.type === 'npub' || decoded.type === 'nprofile'}<nostr-profile
-		display="name"
-		id={part.metadata.plainNip19}
-		{theme}
-	></nostr-profile>{:else if decoded.type === 'nevent' || decoded.type === 'note'}
-	<nostr-note {display} id={part.metadata.plainNip19} {theme}></nostr-note>
-{:else if decoded.type === 'naddr'}
-	<Link {themeClass} href={`https://njump.me/${part.content}`}>{part.content}</Link>
-{:else if decoded.type === 'nsec'}
-	<Link {themeClass} href={`https://njump.me/${part.content}`}>{part.content}</Link>
-{/if}
+<div class="decoded">
+	{#if decoded.type === 'npub' || decoded.type === 'nprofile'}<nostr-profile
+			display="name"
+			id={part.metadata.plainNip19}
+			{theme}
+		></nostr-profile>{:else if decoded.type === 'nevent' || decoded.type === 'note'}
+		<nostr-note {display} id={part.metadata.plainNip19} {theme}></nostr-note>
+	{:else if decoded.type === 'naddr'}
+		<Link {themeClass} href={`https://njump.me/${part.content}`}>{part.content}</Link>
+	{:else if decoded.type === 'nsec'}
+		<Link {themeClass} href={`https://njump.me/${part.content}`}>{part.content}</Link>
+	{/if}
+</div>
+
+<style>
+	.decoded {
+		width: fit-content;
+		display: inline-block;
+	}
+</style>
